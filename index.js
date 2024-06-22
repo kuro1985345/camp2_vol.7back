1b0d7f80668f6817ac1bd6a7fb58ed125de779b9
kuro <kumagae081820032@gmail.com>
2024-06-22 15:51:05 +0900

    変更点

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const app = express();//express のインスタンス
+const port = 3001; // listenするport番号
 
-// CORS設定。異なるURLからでも呼び出せるようにする
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header(
-     "Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept"
-   );
-   next();
- });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
- // jsonを使えるようにする
- app.use(express.json());
-
-// サーバ起動
-const port = process.env.PORT || 5001;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-// DBの代わり
-const user = { id: 1, name: 'Taro'};
-
-// GET /api/user
-app.get('/api/user', (req, res) => {
-   res.send(user);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
